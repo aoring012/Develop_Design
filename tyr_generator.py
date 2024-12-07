2080d1cb65a6f3ca3cec3e267e896bfcf8b77984
Rin Aoyama <oca2204240001@edu.oca.ac.jp>
2024-12-07 16:47:13 +0900

    7

--- tyr_generator.py
+++ tyr_generator.py
@@ -12,3 +12,23 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# try_generator関数のジェネレータ式を使用した実装
+def try_generator():
+    # ジェネレータ式で 1 から 5 の値を生成
+    return (data for data in (1, 2, 3, 4, 5))
+
+
+# main関数
+def main():
+    # ジェネレータ式を直接 for ループに渡す
+    for data in try_generator():
+        print(data)
+        if data > 2:
+            break
+
+
+# main関数を実行
+if __name__ == "__main__":
+    main()
