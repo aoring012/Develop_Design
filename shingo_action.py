0e131497f4aa0db87b04e0ccb200f9e9aff4d871
Rin Aoyama <oca2204240001@edu.oca.ac.jp>
2024-12-07 13:45:55 +0900

    2

--- shingo_action.py
+++ shingo_action.py
@@ -1,3 +1,4 @@
+import sys
 from enum import Enum
 
 
@@ -26,3 +27,20 @@ def act_shingou(color: int) -> Shingou:
     except ValueError:
         # 無効な color が渡された場合、例外を発生させる
         raise ValueError("信号機の色に対応していません")
+
+
+# コマンドライン引数を取得し、処理を行う
+if __name__ == "__main__":
+    try:
+        # コマンドライン引数から色を取得（第1引数が色の値）
+        color_value = int(sys.argv[1])
+
+        # act_shingou を呼び出し
+        act_shingou(color_value)
+
+    except IndexError:
+        print(
+            "引数が不足しています。コマンドライン引数で信号機の色の値を指定してください。"
+        )
+    except ValueError as e:
+        print(e)
